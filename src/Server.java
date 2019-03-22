e19d1e05cf0fda00b3ebf78ae0e8b9f61df56c88
Katarina <petrovick@wit.edu>
2019-03-22 17:57:56 -0400

    1st update

---
+++ src/Server.java
@@ -0,0 +1,37 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+
+	private Socket socket = null;
+	private ServerSocket s = null;
+	private DataInputStream stream = null;
+	
+	public GameServer (int 1234) {
+		try {
+			System.out.println("");
+			s = new ServerSocket(1234);
+			System.out.println("" + s);
+			System.out.println("");
+			socket = s.accept();
+			System.out.println("" + socket);
+			open();
+			
+			boolean finish = false;
+			
+			while (!finish) {
+				try {
+					String box = stream.readUTF();
+					System.out.print(box); // box = box on grid
+					finish = box.equals("");
+				}
+				catch (IOException IOE) {
+					finish=true;
+				}
+			}
+		close();
+		}
+		catch (IOException IOE) {
+		}
+
+}
