73cd24cc6d18192f3e9519b326cca9cc92124fb1
Sarah Kaczynski <kaczynskis@wit.edu>
2019-03-22 17:12:06 -0400

    added client class

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -2,9 +2,11 @@
 import java.util.*;
 
 public class DiceRoller {
-
-	public static void main(String[] args) {
+	public static int roll() {
 		Random rand = new Random();
+		double result = rand.next() * 6;
+		int finalResult = int(result);
+		return finalResult;
 	}
 
 }
