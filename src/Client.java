6f43e4c96c50195d93ea3934e733a33a7e23f7ed
Sarah Kaczynski <kaczynskis@wit.edu>
2019-03-25 10:44:19 -0400

    client base structure complete

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.net.Socket;
+import java.io.*;
 
 public class Client {
-
+	//connect to server
+	public static void main(String[] args) {
+		System.out.println(DiceRoller.roll());
+		/*while(true) {
+			try {
+				Socket s = new Socket("localhost", 1234);
+				System.out.println(DiceRoller.roll());
+			}
+			catch(IOException e) {
+				e.printStackTrace();
+			}
+		}*/
+	}
 }
--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -2,10 +2,13 @@
 import java.util.*;
 
 public class DiceRoller {
+	/*public static void main(String[] args) {
+		System.out.println(DiceRoller.roll());
+	}*/
 	public static int roll() {
 		Random rand = new Random();
-		double result = rand.next() * 6;
-		int finalResult = int(result);
+		double result = rand.nextDouble() * 6;
+		int finalResult = (int)result;
 		return finalResult;
 	}
 
