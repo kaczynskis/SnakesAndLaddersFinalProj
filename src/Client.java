0ce075d6095cd3724b6093b7ff1c11f3dba3cd1e
Sarah Kaczynski <kaczynskis@wit.edu>
2019-04-01 11:33:29 -0400

    edited client class

--- src/Client.java
+++ src/Client.java
@@ -6,16 +6,14 @@ import java.io.*;
 public class Client {
 	//connect to server
 	public static void main(String[] args) {
-		while(true) {
-			try {
-				Socket s = new Socket("localhost", 1234);
-				DataOutputStream out = new DataOutputStream(s.getOutputStream());
-				System.out.println("connected");
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-			System.out.println(DiceRoller.roll());
+		try {
+			Socket s = new Socket("localhost", 1234);
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			System.out.println("connected");
 		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println(DiceRoller.roll());
 	}
 }
