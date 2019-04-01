30582c4a94c67dcd1842bd03f834e047ac681784
Sarah Kaczynski <kaczynskis@wit.edu>
2019-04-01 10:17:43 -0400

    added all BoardPairs

--- src/DiceRoller.java
+++ src/DiceRoller.java
@@ -2,9 +2,6 @@
 import java.util.*;
 
 public class DiceRoller {
-	/*public static void main(String[] args) {
-		System.out.println(DiceRoller.roll());
-	}*/
 	public static int roll() {
 		Random rand = new Random();
 		double result = rand.nextDouble() * 6;
--- src/GameBoard.java
+++ src/GameBoard.java
@@ -10,6 +10,21 @@ public class GameBoard {
 		pairs.add(new BoardPair("ladder", boxes[1], boxes[38]));
 		pairs.add(new BoardPair("ladder", boxes[4], boxes[14]));
 		pairs.add(new BoardPair("ladder", boxes[9], boxes[31]));
-		
+		pairs.add(new BoardPair("ladder", boxes[28], boxes[84]));
+		pairs.add(new BoardPair("ladder", boxes[21], boxes[42]));
+		pairs.add(new BoardPair("ladder", boxes[36], boxes[44]));
+		pairs.add(new BoardPair("ladder", boxes[51], boxes[67]));
+		pairs.add(new BoardPair("ladder", boxes[80], boxes[100]));
+		pairs.add(new BoardPair("ladder", boxes[71], boxes[91]));
+		pairs.add(new BoardPair("snake", boxes[6], boxes[16]));
+		pairs.add(new BoardPair("snake", boxes[11], boxes[49]));
+		pairs.add(new BoardPair("snake", boxes[19], boxes[62]));
+		pairs.add(new BoardPair("snake", boxes[24], boxes[87]));
+		pairs.add(new BoardPair("snake", boxes[26], boxes[48]));
+		pairs.add(new BoardPair("snake", boxes[53], boxes[56]));
+		pairs.add(new BoardPair("snake", boxes[60], boxes[64]));
+		pairs.add(new BoardPair("snake", boxes[73], boxes[93]));
+		pairs.add(new BoardPair("snake", boxes[75], boxes[95]));
+		pairs.add(new BoardPair("snake", boxes[78], boxes[98]));
 	}
 }
